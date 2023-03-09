0741f60bbdf203f995957508e359bb6581d481d0
Nancy <nancyobed11@gmail.com>
2023-03-09 08:36:41 -0600

    Agregar. NoDigasmmdsMJ. clase java de prueba

---
+++ practica/src/practica/Vite.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author labingsw07
+ */
+public class Vite {
+    
+    public String NoDigasmmdsMJ(){
+    return ":v";
+}
+}
