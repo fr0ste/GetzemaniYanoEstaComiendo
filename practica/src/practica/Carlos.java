70cd054f81f8854fd738e3255c77b32e482832bb
CARLOS ALCANTARA <carlos.cantrax303@gmail.com>
2023-03-09 08:36:04 -0600

    Agregar.Carlos:se agrego el metodo dejardecomer

---
+++ practica/src/practica/Carlos.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author labdessw09
+ */
+public class Carlos {
+    public void Dejardecomer(){
+        
+    }
+}
