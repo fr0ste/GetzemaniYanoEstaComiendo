ce26bc7d5c7624538e924919923ee17f61a9af57
tecweb01 <tecweb01@tecweb01-All-Series>
2023-03-09 08:36:01 -0600

    Agregar. login: se agrego el metodo dejar de comer.

---
+++ practica/src/practica/Login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author tecweb01
+ */
+public class Login {
+    
+    
+    public int dejarDeComer(){
+        return 0;
+    }
+    
+}
